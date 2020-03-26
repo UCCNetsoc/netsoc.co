38054bd15f7509c6aef331c4fa1e02c01e595a71
Eric Moynihan <git@moynihan.io>
2020-03-26 19:45:24 +0000

    Added React

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import { render } from "react-dom";
+
+const container = document.querySelector("#root");
+
+if (container) {
+  render(<div>Hello World</div>, container);
+}
