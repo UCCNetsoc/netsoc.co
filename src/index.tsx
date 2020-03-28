3ffe7ec162c7d62ee1779cbb9c610877101f0342
Eric Moynihan <git@moynihan.io>
2020-03-28 18:54:35 +0000

    Added prettierrc for stricter style enforcement

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
-import * as React from "react";
-import { render } from "react-dom";
+import * as React from 'react';
+import { render } from 'react-dom';
 
-const container = document.querySelector("#root");
+const container = document.querySelector('#root');
 
 if (container) {
   render(<div>Hello World</div>, container);
