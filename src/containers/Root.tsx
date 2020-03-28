8d36b7abe9a45aaa56d62b45224b906aaab4cb06
Eric Moynihan <git@moynihan.io>
2020-03-28 18:13:31 +0000

    Added styled components

--- src/containers/Root.tsx
+++ src/containers/Root.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
+import styled from "styled-components";
+
+const Root = styled.div`
+  margin: 0;
+  padding: 0;
+  border: 0;
+`;
 
 export interface RootProps extends React.HTMLAttributes<HTMLDivElement> {}
 
 export default function(props: RootProps): React.ReactElement<RootProps> {
-  return <div {...props}></div>;
+  return <Root {...props}></Root>;
 }
