804dd52954d8ecdc4e2537877c32366ac7588626
Eric Moynihan <git@moynihan.io>
2020-03-29 04:31:51 +0100

    Cover image

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,7 +3,17 @@ import styled from 'styled-components';
 import imgMain from '../../public/img/1.jpg';
 
 interface RootProps {}
-const Root = styled.div<RootProps>``;
+const Root = styled.div<RootProps>`
+  height: 80vh;
+  overflow: hidden;
+  width: 100%;
+`;
+
+const Background = styled.img`
+  height: 100%;
+  width: 100%;
+  object-fit: cover;
+`;
 
 export interface CarouselProps extends RootProps {}
 export default function(
@@ -11,7 +21,7 @@ export default function(
 ): React.ReactElement<CarouselProps> {
   return (
     <Root>
-      <img src={imgMain} alt="" />
+      <Background src={imgMain} alt="" />
     </Root>
   );
 }
