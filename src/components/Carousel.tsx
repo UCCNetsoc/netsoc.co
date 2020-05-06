79e8e4db0a908e53d011d9c779de75b80111ade7
Eric Moynihan <git@moynihan.io>
2020-05-06 19:44:37 +0100

    Added event querying from discord bot

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -124,6 +124,7 @@ interface IEvent {
   title: string;
   description: string;
   image_url: string;
+  date: number;
 }
 
 export interface CarouselProps extends RootProps {}
@@ -150,34 +151,35 @@ export default function (
         <News>
           <h1>Events</h1>
           <div className="eventContainer">
-            <div className="event">
-              <h1>Gamer time</h1>
-              <h2>1/1/1970</h2>
-              <img
-                src="https://scontent-ams4-1.xx.fbcdn.net/v/t1.0-9/87009171_3249079355153927_6271120072882782208_o.jpg?_nc_cat=103&_nc_sid=b386c4&_nc_ohc=ZXXfT7CQbpoAX-wtBKB&_nc_ht=scontent-ams4-1.xx&oh=580504501839e51aec91463abd4310f5&oe=5EB63593"
-                alt=""
-              />
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Dolorem officia assumenda iure libero repellendus ad veritatis
-                possimus, dolores eveniet. Labore laudantium tempora harum
-                dignissimos sequi architecto accusantium itaque natus rerum.
-              </p>
-            </div>
-            <div className="event">
-              <h1>Tech Time</h1>
-              <h2>3/1/1970</h2>
-              <img
-                src="https://scontent-amt2-1.xx.fbcdn.net/v/t1.0-9/83852108_3253488654712997_6761968224167788544_o.jpg?_nc_cat=108&_nc_sid=b386c4&_nc_ohc=v30OxkcNSzoAX8puEc6&_nc_ht=scontent-amt2-1.xx&oh=ec13e3244f1fc8f27af8142177e6416c&oe=5EB4ED2D"
-                alt=""
-              />
-              <p>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit.
-                Dolorem officia assumenda iure libero repellendus ad veritatis
-                possimus, dolores eveniet. Labore laudantium tempora harum
-                dignissimos sequi architecto accusantium itaque natus rerum.
-              </p>
-            </div>
+            {(() => {
+              const output: JSX.Element[] = [];
+              for (let event of events) {
+                try {
+                  const date = new Date(event.date * 1000);
+                  output.push(
+                    <div className="event">
+                      <h1>{event.title}</h1>
+                      <h2>
+                        {(date.getMonth() > 8
+                          ? date.getMonth() + 1
+                          : '0' + (date.getMonth() + 1)) +
+                          '/' +
+                          (date.getDate() > 9
+                            ? date.getDate()
+                            : '0' + date.getDate()) +
+                          '/' +
+                          date.getFullYear()}
+                      </h2>
+                      <img src={event.image_url} alt="" />
+                      <p>{event.description}</p>
+                    </div>
+                  );
+                } catch ({ message }) {
+                  console.error(message);
+                }
+              }
+              return output;
+            })()}
           </div>
         </News>
       </div>
