3dc881f1a2d0f4412e1d510b5cd019ff8a6ca8f6
Eric Moynihan <git@moynihan.io>
2020-05-26 19:38:19 +0100

    Added loading and error display routing for Announcements

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -1,11 +1,25 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import loader from '../../public/img/loader.svg';
 import { API_URL } from '../config';
 
 interface RootProps {}
 const Root = styled.div<RootProps>`
   background: #007bde;
   margin-top: -1px;
+  & .loading {
+    margin-left: calc(50% - 20px);
+    width: 40px;
+    margin-top: 30px;
+    margin-bottom: 30px;
+  }
+  & .error {
+    font-family: 'Lato', sans-serif;
+    color: #fff;
+    font-size: 1.5em;
+    margin-left: 20%;
+    line-height: 100px;
+  }
   & .announce {
     margin-left: 20%;
     border-top: solid 5px #003e7073;
@@ -49,6 +63,8 @@ export default function (
   props: AnnouncementsProps
 ): React.ReactElement<AnnouncementsProps> {
   const [announcements, setAnnouncements] = React.useState<IAnnouncement[]>([]);
+  const [loading, setLoading] = React.useState(true);
+  const [error, setError] = React.useState('');
   React.useEffect(() => {
     (async () => {
       try {
@@ -56,46 +72,56 @@ export default function (
         const recieved = (await data.json()) as IAnnouncement[];
         setAnnouncements(recieved);
         console.log(recieved);
+        if (recieved.length === 0) {
+          setError('No announcements currently available');
+        }
       } catch ({ message }) {
-        console.error(message);
+        setError('An error occurred fetching announcements');
       }
+      setLoading(false);
     })();
   }, []);
   return (
     <Root>
       <section className="eventContainer">
-        {(() => {
-          const output: JSX.Element[] = [];
-          for (let announce of announcements) {
-            try {
-              const date = new Date(announce.date * 1000);
-              const ye = new Intl.DateTimeFormat('en', {
-                year: 'numeric',
-              }).format(date);
-              const mo = new Intl.DateTimeFormat('en', {
-                month: 'short',
-              }).format(date);
-              const da = new Intl.DateTimeFormat('en', {
-                day: '2-digit',
-              }).format(date);
-              const element = (
-                <div key={announce.date} className="announce">
-                  {announce.image_url && (
-                    <img src={announce.image_url} alt="" />
-                  )}
-                  <h1>{`${da} ${mo} ${ye}`}</h1>
-                  <p className={announce.image_url && 'imgContent'}>
-                    {announce.content}
-                  </p>
-                </div>
-              );
-              output.push(element);
-            } catch ({ message }) {
-              console.error(message);
+        {loading ? (
+          <img className="loading" src={loader} alt="Loading" />
+        ) : error ? (
+          <span className="error">{error}</span>
+        ) : (
+          (() => {
+            const output: JSX.Element[] = [];
+            for (let announce of announcements) {
+              try {
+                const date = new Date(announce.date * 1000);
+                const ye = new Intl.DateTimeFormat('en', {
+                  year: 'numeric',
+                }).format(date);
+                const mo = new Intl.DateTimeFormat('en', {
+                  month: 'short',
+                }).format(date);
+                const da = new Intl.DateTimeFormat('en', {
+                  day: '2-digit',
+                }).format(date);
+                const element = (
+                  <div key={announce.date} className="announce">
+                    {announce.image_url && (
+                      <img src={announce.image_url} alt="" />
+                    )}
+                    <h1>{`${da} ${mo} ${ye}`}</h1>
+                    <p className={announce.image_url && 'imgContent'}>
+                      {announce.content}
+                    </p>
+                  </div>
+                );
+                output.push(element);
+              } catch ({ message }) {
+                console.error(message);
+              }
             }
-          }
-          return output;
-        })()}
+            return output;
+          })()
+        )}
       </section>
     </Root>
   );
