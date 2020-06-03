5cf92c513d03cf34cf0f9882273cccca0458b5d1
Eric Moynihan <git@moynihan.io>
2020-06-03 13:09:36 +0100

    Styled gallery

--- src/containers/Gallery.tsx
+++ src/containers/Gallery.tsx
@@ -15,9 +15,24 @@ const Root = styled.div`
   background: #111;
   border-top: solid 5px #111;
   margin-top: -1px;
+  & .pswp-thumbnails {
+    text-align: center;
+    padding: 0 10%;
+  }
+  & .pswp-thumbnail {
+    display: inline-block;
+  }
+  & .pswp img {
+    max-width: none;
+    margin-left: auto;
+    margin-right: auto;
+    left: 0;
+    right: 0;
+    text-align: center;
+  }
 `;
 const Thumb = styled.img`
-  height: 200px;
+  height: 250px;
   width: auto;
   cursor: pointer;
 `;
@@ -34,27 +49,36 @@ export default function (
         const response = (await (await fetch(GITHUB_URL)).json()) as {
           name: string;
           download_url: string;
+          type: string;
+          path: string;
         }[];
+        console.log(response);
+        const tempArray: PhotoSwipeGalleryItem[] = [];
         for (let img of response) {
-          let block = new Image();
-          block.src = img.download_url;
-          block.onload = () => {
-            let thumbIndex = img.download_url.lastIndexOf('/');
-            let thumbnail = [
-              img.download_url.slice(0, thumbIndex),
-              '/thumbnails',
-              img.download_url.slice(thumbIndex),
-            ].join('');
-            let item = {
-              src: img.download_url,
-              thumbnail,
-              title: img.name.split('.')[0].replace('_', ' '),
-              w: block.width,
-              h: block.height,
-            } as PhotoSwipeGalleryItem;
-            setItems(items.concat(item));
-          };
+          console.log(img);
+          if (img.type !== 'file') {
+            continue;
+          }
+          let thumbIndex = img.download_url.lastIndexOf('/');
+          let thumbnail = [
+            img.download_url.slice(0, thumbIndex),
+            '/thumbnails',
+            img.download_url.slice(thumbIndex),
+          ].join('');
+          try {
+            await fetch(thumbnail);
+          } catch {
+            thumbnail = img.download_url;
+          }
+          let item = {
+            src: img.download_url,
+            thumbnail,
+            title: img.name.split('.')[0].replace('_', ' '),
+            h: window.outerHeight,
+          } as PhotoSwipeGalleryItem;
+          tempArray.push(item);
         }
+        setItems(tempArray);
       } catch ({ message }) {
         console.error(message);
       }
@@ -72,6 +96,7 @@ export default function (
           isOpen={isOpen}
           items={items}
           options={{}}
+          onClose={() => setIsOpen(false)}
         ></PhotoSwipeGallery>
       </Root>
     </div>
