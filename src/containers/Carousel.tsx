7f55be5f238ad171f90510bc5f7f5017c4f3e9b7
Eric Moynihan <git@moynihan.io>
2020-11-09 20:47:25 +0000

    Added date fix for website

--- src/containers/Carousel.tsx
+++ src/containers/Carousel.tsx
@@ -229,13 +229,13 @@ export default function (
                         <div key={event.date} className="event">
                           <h1>{event.title}</h1>
                           <h2>
-                            {(date.getMonth() > 8
-                              ? date.getMonth() + 1
-                              : '0' + (date.getMonth() + 1)) +
+                            {(date.getDate() > 9
+                              ? date.getDate()
+                              : '0' + date.getDate()) +
                               '/' +
-                              (date.getDate() > 9
-                                ? date.getDate()
-                                : '0' + date.getDate()) +
+                              (date.getMonth() > 8
+                                ? date.getMonth() + 1
+                                : '0' + date.getMonth() + 1) +
                               '/' +
                               date.getFullYear()}
                           </h2>
