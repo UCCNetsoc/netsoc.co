6a13337021d6f795576ee2b88e7f74a356b1741d
Meabh Doyle <meabhlizdoyle@gmail.com>
2022-10-05 18:16:04 +0100

    added homepage and styles

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,8 +1,26 @@
+import { Link } from "react-router-dom";
+import styles from "./Home.module.css";
 const Home = () => {
   return (
-    <>
-      <h1>Home</h1>
-    </>
+
+    <main>
+      <nav className={styles.nav}>
+        <ul>
+          <li>
+            <Link to="/about" className={styles.btn}>About</Link>
+          </li>
+          <li>
+            <Link to="/about" className={styles.btn}>Technology</Link>
+          </li>
+          <li>
+            <Link to="/about" className={styles.btn}>Gaming</Link>
+          </li>
+          <li>
+            <Link to="/about" className={styles.btn}>Esports</Link>
+          </li>
+        </ul>
+      </nav>
+    </main>
   )
 }
 
