4777331b01bb7443f0f4abce48a7e2c5bfe9998e
Colm Murphy <colmmurphy016@gmail.com>
2022-11-05 14:31:29 +0000

    had to change imports to run app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './views/Home';
+// import Home from './views/Home';
+import Home from './views/Home/Home';   // app won't run unless i add an extra /Home ¯\_(ツ)_/¯
 
 function App() {
   return (
