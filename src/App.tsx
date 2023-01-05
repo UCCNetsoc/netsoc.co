c34ac6d68709f81e43e424263f1aec2a1afb2655
Thomas Daniel Galligan <thomas007g@gmail.com>
2023-01-05 15:58:04 +0000

    added misc changes with Reece

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import About from './views/About/About';
 import Home from './views/Home/Home';
+import Header from './components/Header';
 
 function App() {
   return (
     <>
       <Router>
-        <header>
-        </header>
+        <header/>
         <Routes>
           <Route path="/about" element={<About />} />
           <Route path="/" element={<Home />} />
