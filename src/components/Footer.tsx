2a47778de73c107c30ee006e2b2164affbdc3808
Colm Murphy <colmmurphy016@gmail.com>
2023-01-20 14:42:41 +0000

    add alt text to images in footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,22 +2,26 @@ import styles from './Footer.module.css'
 
 const Footer = (): JSX.Element => {
 
-  const footer_links: { icon?: string, url: string }[] = [
+  const footer_links: { icon?: string, url: string, description: string }[] = [
     {
         icon: "/img/discord.svg",
-        url: "https://discord.netsoc.co"
+        url: "https://discord.netsoc.co",
+        description: "Join our Discord server"
       }, 
       {
         icon: "/img/google_calendar.png",
-        url: "https://google.com"
+        url: "https://google.com",
+        description: "See what events we have coming up"
       },
       {
         icon: "https://instagram.com/favicon.ico",
-        url: "https://instagram.com/uccnetsoc"
+        url: "https://instagram.com/uccnetsoc",
+        description: "Follow us on instagram to be updated with upcoming events"
       },
       {
         icon: "/img/github.svg",
-        url: "https://github.com/uccnetsoc"
+        url: "https://github.com/uccnetsoc",
+        description: "Check out our Github"
       },
   ]
 
@@ -26,7 +30,7 @@ const Footer = (): JSX.Element => {
       <footer>
           <section className={styles.footer_links}>
               {footer_links.map((link) => (
-                  <a href={link.url}><img src={link.icon} /></a>
+                  <a href={link.url}><img src={link.icon} alt={link.description}/></a>
               ))}
           </section>
           <iframe src="https://discord.com/widget?id=248421213628530688&theme=dark" width="350" height="500" allowTransparency={true} 
