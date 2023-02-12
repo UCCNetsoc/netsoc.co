1a332efa804b3eb3a5607942614459cb938ae056
virtus <AleksySi2509@protonmail.com>
2023-02-12 01:06:54 +0000

    Added more content to the Technology page

--- src/views/Technology/Technology.tsx
+++ src/views/Technology/Technology.tsx
@@ -1,12 +1,50 @@
 import styles from "./Technology.module.css";
 
+/* MORE CONTENT NEEDS TO BE ADDED :)⠀⠀⠀⠀⠀⠀⠀
+        ⣠⣤⣤⣤⣤⣤⣄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+      ⢰⡿⠋⠁⠀⠀⠈⠉⠙⠻⣷⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢀⣿⠇⠀⢀⣴⣶⡾⠿⠿⠿⢿⣿⣦⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⣀⣀⣸⡿⠀⠀⢸⣿⣇⠀⠀⠀⠀⠀⠀⠙⣷⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⣾⡟⠛⣿⡇⠀⠀⢸⣿⣿⣷⣤⣤⣤⣤⣶⣶⣿⠇⠀⠀⠀⠀⠀⠀⠀⣀⠀⠀
+⢀⣿⠀⢀⣿⡇⠀⠀⠀⠻⢿⣿⣿⣿⣿⣿⠿⣿⡏⠀⠀⠀⠀⢴⣶⣶⣿⣿⣿⣆
+⢸⣿⠀⢸⣿⡇⠀⠀⠀⠀⠀⠈⠉⠁⠀⠀⠀⣿⡇⣀⣠⣴⣾⣮⣝⠿⠿⠿⣻⡟
+⢸⣿⠀⠘⣿⡇⠀⠀⠀⠀⠀⠀⠀⣠⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠁⠉⠀
+⠸⣿⠀⠀⣿⡇⠀⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠟⠉⠀⠀⠀⠀
+⠀⠻⣷⣶⣿⣇⠀⠀⠀⢠⣼⣿⣿⣿⣿⣿⣿⣿⣛⣛⣻⠉⠁⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢸⣿⠀⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⢸⣿⣀⣀⣀⣼⡿⢿⣿⣿⣿⣿⣿⡿⣿⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⠙⠛⠛⠛⠋⠁⠀⠙⠻⠿⠟⠋⠑⠛⠋  */
+
 const Technology = () => {
     return (
         <>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                  cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            <div className={styles.bg}></div>
+            <main className={styles.main}>
+                <article className={styles.article}>
+                    <section>
+                        <h1 className={styles.title}>Netsoc Cloud</h1>
+                        <p>We offer students many services ranging from gamerservers to web hosting.</p>
+                    </section>
+                    <section>
+                        <h2>Web Services</h2>
+                        <p>Easily deploy a web service with a few clicks.
+                            Pre-configured templates include WordPress, Ghost CMS, static hosting and more</p>
+                    </section>
+                    <section>
+                        <h2>Gameservers</h2>
+                        <p>Spin up a game server and play games like Minecraft with your friends in seconds</p>
+                    </section>
+                    <section>
+                        <h2>Development Tools</h2>
+                        <p>Boot a container with support for any programming language you can think of</p>
+                    </section>
+                    <section>
+                        <h2>Virtual Private Servers</h2>
+                        <p>Launch a Linux host like Debian / Ubuntu / Arch / CentOS and run tools like Docker,
+                            LXD and more</p>
+                    </section>
+                </article>
+            </main>
         </>
     )
 }
