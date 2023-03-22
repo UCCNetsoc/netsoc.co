b5e9c77182fca8af4e8f36206b5727dcc735e30b
Thomas Daniel Galligan <thomas007g@gmail.com>
2023-03-22 14:12:09 +0000

    update routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,27 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-// import Home from './views/Home';
+import Footer from './components/Footer';
+
 import Home from './views/Home/Home';   // app won't run unless i add an extra /Home ¯\_(ツ)_/¯
+import About from './views/Home/About';
+import Gaming from './views/Home/Gaming';
+import Technology from './views/Home/Technology';
 
 function App() {
   return (
     <>
-      <Router>
-        <header>
-        </header>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <div style={{ minHeight: "74vh"}}>
+        <Router>
+          <Routes>
+            <Route path="/about" element={<About />} />
+            <Route path="/tech" element={<Technology />} />
+            <Route path="/technology" element={<Technology />} />
+            <Route path="/gaming" element={<Gaming />} />
+            <Route path="/esports" element={<Gaming />} />
+            <Route path="/" element={<Home />} />
+          </Routes>
+        </Router>
+      </div>
+      <Footer />
     </>
   );
 }
