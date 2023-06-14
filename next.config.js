1ac995b006ae113040587bc91f0379805e738889
Thomas Galligan <thomas007galligan@gmail.com>
2023-06-14 15:37:38 +0100

    add next page redirect

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/about",
+        permanent: true,
+      },
+    ];
+  },
+}
 
 module.exports = nextConfig
