cc7835139e70990804118c9c37ce2c9e0874cc3e
Thomas Galligan <thomas007galligan@gmail.com>
2023-06-14 15:36:14 +0100

    add css colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'brand-primary': '#353332',
+        'brand-secondary': '#fffeff',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
